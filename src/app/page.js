1437498e9e090e789b76e81a314f3950250a804a
FojleTuhin <fojletuhin@gmail.com>
2025-02-22 18:41:54 +0600

    add skills

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 import TextMarque from "@/components/TextMarque";
 import WhoAmI from "@/components/WhoAmI";
 
@@ -11,6 +13,8 @@ export default function Home() {
       <Navbar />
       {/* <Hero /> */}
       <WhoAmI />
+      <Skills />
+      <Projects />
       <TextMarque />
       <Contact />
       <Footer />
---
+++ src/components/Projects.jsx
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import React from "react";
+
+const Projects = () => {
+  const projects = [
+    {
+      no: "1",
+      title: "Nutritionist",
+      description: "It's a Personalized Nutrition Coaching based website.",
+      tech: "Tailwind, ShadCn, Next Js.",
+      link: "https://nutritionisttuhin.vercel.app/",
+    },
+    {
+      no: "2",
+      title: "ParcelPioneer",
+      description: "It's a Parcel Delivery and management website.",
+      tech: "React, Node Js., Express Js., MongoDB, Stripe",
+      link: "https://parcelpioneer.netlify.app/",
+    },
+    {
+      no: "3",
+      title: "InsureCow",
+      description: "It's a Parcel Delivery and management website.",
+      tech: "Tailwind, ShadCn, Next Js.",
+      link: "https://insure-cow.vercel.app/",
+    },
+    {
+      no: "4",
+      title: "ZEP",
+      description: "It's a Agency based website.",
+      tech: "Tailwind, ShadCn, Next Js.",
+      link: "https://zep.vercel.app/",
+    },
+  ];
+  return (
+    <div className="max-w-[1320px] mx-auto pb-[196px]">
+      <div className="grid grid-cols-2 mb-16 px-6">
+        <p className="text-[112px] font-medium leading-none">MY PROJECTS</p>
+        <div className="flex  justify-end items-end">
+          <p className="text-[58px] font-medium leading-none ">
+            45+ <br /> PROJECTS <br /> COMPLETED
+          </p>
+        </div>
+      </div>
+
+      {projects.map((project, index) => (
+        <a href={project.link} key={index} target="_blank" rel="noreferrer">    
+          <div className="group py-12 px-6 border-y border-[#F1CCFF] flex justify-between gap-[18px]  duration-700 transition-all ease-in-out hover:bg-[#F1CCFF] hover:cursor-[#000000]">
+            <p className="flex items-center w-[200px] group-hover:text-black">
+              {project.no}/
+            </p>
+            <div className="flex-1">
+              <div className="flex justify-between gap-4">
+                <div>
+                  <p className="text-[48px] font-medium text-[#F1CCFF] group-hover:text-black leading-none">
+                    {project.title}
+                  </p>
+                  <p className="mt-3 text-[#FFFFFFB8] group-hover:text-[#000000B8] leading-none">
+                    {project.description}
+                  </p>
+                </div>
+                <div className="flex items-end text-end">
+                  <p className="group-hover:text-black">{project.tech}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="w-[200px] flex justify-end items-end">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                viewBox="0 0 100 100"
+                fill="none"
+                className="transition-all duration-700 ease-in-out transform group-hover:rotate-45"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M86.2052 8.08081H0V0H100V100H91.9192V13.7948L5.71399 100L0 94.286L86.2052 8.08081Z"
+                  className="fill-white transition-colors duration-300 ease-in-out group-hover:fill-black"
+                />
+              </svg>
+            </div>
+          </div>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
