f047e6a9a546ad2571b1171f829c3e8cc2d75dbe
FojleTuhin <fojletuhin@gmail.com>
2025-02-23 01:12:07 +0600

    update

--- src/components/TextMarque.jsx
+++ src/components/TextMarque.jsx
@@ -5,12 +5,32 @@ import Image from "next/image";
 const TextMarque = () => {
   return (
     <div className="py-16 bg-[#F1CCFF]">
-      <Marquee>
-        <div className="flex items-center gap-16">
-          <p className="text-[130px] font-medium text-[#000000]">
-            OPEN FOR WORK
-          </p>
-          <Image src={img} alt="sunflower" className="" />
+      <Marquee speed={100}>
+        <div className="flex gap-16">
+          <div className="flex items-center gap-16">
+            <p className="text-[130px] font-medium text-[#000000]">
+              OPEN FOR WORK
+            </p>
+            <Image src={img} alt="sunflower" className="" />
+          </div>
+          <div className="flex items-center gap-16">
+            <p className="text-[130px] font-medium text-[#000000]">
+              OPEN FOR WORK
+            </p>
+            <Image src={img} alt="sunflower" className="" />
+          </div>
+          <div className="flex items-center gap-16">
+            <p className="text-[130px] font-medium text-[#000000]">
+              OPEN FOR WORK
+            </p>
+            <Image src={img} alt="sunflower" className="" />
+          </div>
+          <div className="flex items-center gap-16">
+            <p className="text-[130px] font-medium text-[#000000]">
+              OPEN FOR WORK
+            </p>
+            <Image src={img} alt="sunflower" className="" />
+          </div>
         </div>
       </Marquee>
     </div>
