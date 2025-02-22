5b0e9d6abf70e71a5b685310d899fc7b316138b4
FojleTuhin <fojletuhin@gmail.com>
2025-02-22 16:13:33 +0600

    ad footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,51 @@
+import { ArrowUp } from "lucide-react";
+import Image from "next/image";
+import vector from "../../public/vector.svg";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#F1CCFF] rounded-[12px] max-w-[1094px]  mx-auto py-40 px-28">
+      <div className="grid grid-cols-2 gap-[18px]">
+        <div className="flex flex-col justify-between h-[322px]">
+          <div className="flex">
+            <button className="bg-[#0000001A] text-[#000000] rounded-[12px] py-5 px-6 flex gap-[10px]">
+              <p className="text-[18px] font-medium">Back to top</p>
+              <ArrowUp />
+            </button>
+          </div>
+
+          <Image src={vector} alt="logo" className="w-[324px] h-auto" />
+        </div>
+
+        <div className="text-[#000000] flex flex-col justify-between h-[322px]">
+          <div>
+            <p className="text-[18px] font-medium mb-12">
+              Built with passion & code.
+            </p>
+            <p className="mb-[10px] text-[18px] font-medium underline cursor-pointer">
+              Skills
+            </p>
+            <p className="mb-[10px] text-[18px] font-medium underline cursor-pointer">
+              Projects
+            </p>
+          </div>
+
+          <div>
+            <a
+              href="mailto:fojletuhin@gmail.com"
+              className="text-[32px] font-medium mb-6"
+            >
+              fojletuhin@gmail.com
+            </a>
+
+            <p className="pt-3 border-t border-black text-[#000000] text-[18px] font-medium ">
+              © Fojle Rabbi Tuhin 2025. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
